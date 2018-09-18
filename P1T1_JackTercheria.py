15e91107fbca7439991a93db0e61ea4378a8de56
JackTercheria <43195883+JackTercheria@users.noreply.github.com>
2018-09-18 19:21:28 -0400

    Add files via upload
    My first python program...

---
+++ P1T1_JackTercheria.py
@@ -0,0 +1,10 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> # A brief description of the project
+>>> # 8/25/2018
+>>> # CTI-110 Tutorial 1 - My First Python Program
+>>> # Jack Tercheria
+>>> #
+>>> print('This is a test of IDLE.')
+This is a test of IDLE.
+>>> 
